10196e477c9db631116d17b43019c3970613ad40
Kriszta <144325460+KrisztaAntal@users.noreply.github.com>
2025-01-14 10:21:36 +0100

    add parameters and constraints to NewMemberRequest record

--- backend/src/main/java/org/coathangerstudios/backend/model/payload/NewMemberRequest.java
+++ backend/src/main/java/org/coathangerstudios/backend/model/payload/NewMemberRequest.java
@@ -1,4 +1,18 @@
 package org.coathangerstudios.backend.model.payload;
 
-public record NewMemberRequest() {
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public record NewMemberRequest(@NotBlank(message = "Username cannot be blank") String username,
+                               @NotBlank(message = "First name cannot be blank") String firstName,
+                               @NotBlank(message = "Last name cannot be blank") String lastName,
+                               @NotBlank(message = "Password cannot be blank") @Size(message = "Password must be 8-20 characters long", min = 8, max = 20) String password,
+                               @Email(message = "Email address must be a valid email format") String email,
+                               @NotBlank @Pattern(
+                                       regexp = "^\\d{4}-\\d{2}-\\d{2}$",
+                                       message = "Birthdate must be in the format yyyy-MM-dd"
+                               ) String birthdate
+) {
 }
