7282f59f305d189d0f8970ca6b7986228e955e9f
Kriszta <144325460+KrisztaAntal@users.noreply.github.com>
2025-01-17 14:09:32 +0100

    add login and signup endpoints in MemberController

--- backend/src/main/java/org/coathangerstudios/backend/controller/MemberController.java
+++ backend/src/main/java/org/coathangerstudios/backend/controller/MemberController.java
@@ -1,8 +1,11 @@
 package org.coathangerstudios.backend.controller;
 
 import jakarta.validation.Valid;
-import org.coathangerstudios.backend.model.entity.Member;
+import org.coathangerstudios.backend.model.dto.MemberDto;
+import org.coathangerstudios.backend.model.payload.JwtResponse;
+import org.coathangerstudios.backend.model.payload.MemberLoginRequest;
 import org.coathangerstudios.backend.model.payload.NewMemberRequest;
+import org.coathangerstudios.backend.service.MemberService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberController {
 
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
     @PostMapping("/signup")
-    public void signup(@Valid @RequestBody NewMemberRequest newMemberRequest) {
+    public MemberDto signup(@Valid @RequestBody NewMemberRequest newMemberRequest) {
+        return memberService.signUp(newMemberRequest);
+    }
+
+    @PostMapping("/login")
+    public JwtResponse login(@RequestBody MemberLoginRequest memberLoginRequest){
+        return memberService.login(memberLoginRequest);
     }
 }
