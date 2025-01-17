51cb5137160db6e60aeed2b51b7ba10e67f215cf
Kriszta <144325460+KrisztaAntal@users.noreply.github.com>
2025-01-17 14:07:54 +0100

    can send JWT response with JWT token, username and set of roles

--- backend/src/main/java/org/coathangerstudios/backend/model/payload/JwtResponse.java
+++ backend/src/main/java/org/coathangerstudios/backend/model/payload/JwtResponse.java
@@ -1,2 +1,6 @@
-package org.coathangerstudios.backend.model.payload;public record JwtResponse() {
+package org.coathangerstudios.backend.model.payload;
+
+import java.util.Set;
+
+public record JwtResponse(String token, String username, Set<String> roles) {
 }
