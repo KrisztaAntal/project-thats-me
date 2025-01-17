c939883058b8944776281b4955cc025409d11a3e
Kriszta <144325460+KrisztaAntal@users.noreply.github.com>
2025-01-17 13:42:32 +0100

    override loadUserByUsername in UserDetailsService to accept emailaddress too instead of just username

--- backend/src/main/java/org/coathangerstudios/backend/security/service/UserDetailsServiceImpl.java
+++ backend/src/main/java/org/coathangerstudios/backend/security/service/UserDetailsServiceImpl.java
@@ -1,14 +1,47 @@
 package org.coathangerstudios.backend.security.service;
 
+import org.coathangerstudios.backend.model.entity.Member;
+import org.coathangerstudios.backend.model.entity.MemberRole;
+import org.coathangerstudios.backend.repository.MemberRepository;
+import org.coathangerstudios.backend.security.EmailAddressNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String usernameOrEmail) {
+
+        Member member = getMemberFromDatabase(usernameOrEmail);
+
+        List<SimpleGrantedAuthority> roles = new ArrayList<>();
+        for (MemberRole role : member.getRoles()) {
+            roles.add(new SimpleGrantedAuthority(role.getRole().name()));
+        }
+
+        return new User(member.getUsername(), member.getPassword(), roles);
+    }
+
+    private Member getMemberFromDatabase(String usernameOrEmail) {
+        if (usernameOrEmail.contains("@")) {
+            return memberRepository.findUserByEmail(usernameOrEmail).orElseThrow(() -> new EmailAddressNotFoundException(usernameOrEmail));
+        }
+        return memberRepository.findUserByUsername(usernameOrEmail).orElseThrow(() -> new UsernameNotFoundException(usernameOrEmail));
     }
 }
