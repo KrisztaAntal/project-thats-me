79222f9624c30c55b7c54a76446a2f75a12ef084
Kriszta <144325460+KrisztaAntal@users.noreply.github.com>
2025-01-19 17:37:02 +0100

    create AgeLimitValidator, to allow registering of only users over 14 years old

--- backend/src/main/java/org/coathangerstudios/backend/security/customValidator/ageLimit/AgeLimit.java
+++ backend/src/main/java/org/coathangerstudios/backend/security/customValidator/ageLimit/AgeLimit.java
@@ -1,4 +1,4 @@
-package org.coathangerstudios.backend.security.customValidator;
+package org.coathangerstudios.backend.security.customValidator.ageLimit;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
@@ -11,7 +11,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = AgeLimitValidator.class)
-public interface AgeLimit {
+public @interface AgeLimit {
     int minimumAge() default 14;
     String message() default "User must be at least 14 years old";
     Class<?>[] groups() default {};
