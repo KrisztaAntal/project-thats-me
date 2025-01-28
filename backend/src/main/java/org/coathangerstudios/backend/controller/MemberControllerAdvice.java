3437b53a640e11bee344ba20a63e9a1f0f15c466
Kriszta <144325460+KrisztaAntal@users.noreply.github.com>
2025-01-28 19:12:02 +0100

    refactor exception handling in controller advice and in userDetailsService on wrong credentials(username or email)

--- backend/src/main/java/org/coathangerstudios/backend/controller/MemberControllerAdvice.java
+++ backend/src/main/java/org/coathangerstudios/backend/controller/MemberControllerAdvice.java
@@ -1,11 +1,9 @@
 package org.coathangerstudios.backend.controller;
 
-import org.coathangerstudios.backend.exception.EmailAddressAlreadyInUseException;
-import org.coathangerstudios.backend.exception.EmailAddressNotFoundException;
-import org.coathangerstudios.backend.exception.UsernameIsAlreadyInUseException;
+import org.coathangerstudios.backend.exception.UsernameOrEmailAddressAlreadyInUseException;
+import org.coathangerstudios.backend.exception.MemberNotFoundWithGivenCredentialsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,30 +14,16 @@ import java.util.Map;
 public class MemberControllerAdvice {
 
     @ResponseBody
-    @ExceptionHandler(UsernameIsAlreadyInUseException.class)
+    @ExceptionHandler(UsernameOrEmailAddressAlreadyInUseException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public String handleUsernameIsAlreadyInUseException(UsernameIsAlreadyInUseException e) {
+    public String handleUsernameOrEmailAddressIsAlreadyInUseException(UsernameOrEmailAddressAlreadyInUseException e) {
         return e.getMessage();
     }
 
     @ResponseBody
-    @ExceptionHandler(EmailAddressAlreadyInUseException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public String handleEmailAddressIsAlreadyInUseException(EmailAddressAlreadyInUseException e) {
-        return e.getMessage();
-    }
-
-    @ResponseBody
-    @ExceptionHandler(EmailAddressNotFoundException.class)
+    @ExceptionHandler(MemberNotFoundWithGivenCredentialsException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleEmailAddressNotFoundException(EmailAddressNotFoundException e) {
-        return e.getMessage();
-    }
-
-    @ResponseBody
-    @ExceptionHandler(UsernameNotFoundException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleUsernameNotFoundException(UsernameNotFoundException e) {
+    public String handleEmailAddressNotFoundException(MemberNotFoundWithGivenCredentialsException e) {
         return e.getMessage();
     }
 
--- backend/src/main/java/org/coathangerstudios/backend/security/service/UserDetailsServiceImpl.java
+++ backend/src/main/java/org/coathangerstudios/backend/security/service/UserDetailsServiceImpl.java
@@ -3,13 +3,12 @@ package org.coathangerstudios.backend.security.service;
 import org.coathangerstudios.backend.model.entity.Member;
 import org.coathangerstudios.backend.model.entity.MemberRole;
 import org.coathangerstudios.backend.repository.MemberRepository;
-import org.coathangerstudios.backend.exception.EmailAddressNotFoundException;
+import org.coathangerstudios.backend.exception.MemberNotFoundWithGivenCredentialsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -40,8 +39,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     private Member getMemberFromDatabase(String usernameOrEmail) {
         if (usernameOrEmail.contains("@")) {
-            return memberRepository.findUserByEmail(usernameOrEmail).orElseThrow(() -> new EmailAddressNotFoundException(usernameOrEmail));
+            return memberRepository.findUserByEmail(usernameOrEmail).orElseThrow(MemberNotFoundWithGivenCredentialsException::new);
         }
-        return memberRepository.findUserByUsername(usernameOrEmail).orElseThrow(() -> new UsernameNotFoundException(usernameOrEmail));
+        return memberRepository.findUserByUsername(usernameOrEmail).orElseThrow(MemberNotFoundWithGivenCredentialsException::new);
     }
 }
