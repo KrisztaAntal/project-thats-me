239b9b82492b83e294c9ac84f765ac385e6d1ca7
Kriszta <144325460+KrisztaAntal@users.noreply.github.com>
2025-01-28 23:02:20 +0100

    test login with integration tests

--- backend/src/test/java/org/coathangerstudios/backend/serviceTest/MemberIT.java
+++ backend/src/test/java/org/coathangerstudios/backend/serviceTest/MemberIT.java
@@ -2,14 +2,13 @@ package org.coathangerstudios.backend.serviceTest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.coathangerstudios.backend.model.entity.Member;
-import org.coathangerstudios.backend.model.entity.MemberRole;
-import org.coathangerstudios.backend.model.entity.Monogram;
-import org.coathangerstudios.backend.model.entity.Role;
+import org.coathangerstudios.backend.exception.MemberNotFoundWithGivenCredentialsException;
+import org.coathangerstudios.backend.exception.UsernameOrEmailAddressAlreadyInUseException;
+import org.coathangerstudios.backend.model.payload.MemberLoginRequest;
 import org.coathangerstudios.backend.model.payload.NewMemberRequest;
 import org.coathangerstudios.backend.repository.MemberRepository;
-import org.coathangerstudios.backend.repository.MonogramRepository;
 import org.coathangerstudios.backend.repository.MemberRoleRepository;
+import org.coathangerstudios.backend.repository.MonogramRepository;
 import org.coathangerstudios.backend.security.jwt.JwtUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,8 +19,10 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -52,13 +53,6 @@ public class MemberIT {
 
     @BeforeEach
     public void setup() {
-/*        monogramRepository.deleteAll();
-        memberRepository.deleteAll();
-        memberRoleRepository.deleteAll();*/
-        //MemberRole userRole = new MemberRole(Role.ROLE_USER);
-        //memberRoleRepository.save(userRole);
-        //Monogram monogram = new Monogram("FL", "#000000");
-        //Member savedMember = new Member("username", "firstName", "lastName", "$2a$10$AzhoXWmxaTrVM02m2NqM9..NHHQubT9JVFQLMQHXJasxLqFbgyKVC", "email@example.com", LocalDate.parse("2001-11-11"), "", monogram, "", "");
     }
 
     @Test
@@ -72,6 +66,7 @@ public class MemberIT {
                 .andDo(print())
                 .andExpect(status().isOk());
     }
+
     @Test
     public void testSignUp_FailedWithValidators() throws Exception {
         NewMemberRequest newMemberRequest = new NewMemberRequest("username@", "firstName", "lastName", "4564", "emailexample.com", LocalDate.parse("2021-11-11"));
@@ -88,4 +83,87 @@ public class MemberIT {
                 .andExpect(jsonPath("$.birthdate").value("User must be at least 14 years old"))
                 .andDo(print());
     }
+
+    @Test
+    public void testSignUp_FailedBecauseUsernameIsAlreadyInDB() throws Exception {
+        NewMemberRequest newMemberRequest = new NewMemberRequest("ankriszt", "Kriszta", "Antal", "456456456456", "Kriszta12@gmail.com", LocalDate.parse("2004-12-10"));
+
+        mockMvc.perform(post("/api/signup")
+                        .content(jacksonObjectMapper.writeValueAsString(newMemberRequest))
+                        .contentType("application/json;charset=UTF-8")
+                )
+                .andDo(print())
+                .andExpect(status().isConflict())
+                .andExpect(result -> assertInstanceOf(UsernameOrEmailAddressAlreadyInUseException.class, result.getResolvedException()))
+                .andExpect(result -> assertEquals("Username or email address is already in use, please choose another one", Objects.requireNonNull(result.getResolvedException()).getMessage()));
+    }
+
+    @Test
+    public void testSignUp_FailedBecauseEmailIsAlreadyInDB() throws Exception {
+        NewMemberRequest newMemberRequest = new NewMemberRequest("ankriszt12", "Kriszta", "Antal", "456456456456", "Kriszta@gmail.com", LocalDate.parse("2004-12-10"));
+
+        mockMvc.perform(post("/api/signup")
+                        .content(jacksonObjectMapper.writeValueAsString(newMemberRequest))
+                        .contentType("application/json;charset=UTF-8")
+                )
+                .andDo(print())
+                .andExpect(status().isConflict())
+                .andExpect(result -> assertInstanceOf(UsernameOrEmailAddressAlreadyInUseException.class, result.getResolvedException()))
+                .andExpect(result -> assertEquals("Username or email address is already in use, please choose another one", Objects.requireNonNull(result.getResolvedException()).getMessage()));
+    }
+
+    @Test
+    public void testSignUp_FailedBecauseEmailAndUsernameIsAlreadyInDB() throws Exception {
+        NewMemberRequest newMemberRequest = new NewMemberRequest("ankriszt", "Kriszta", "Antal", "456456456456", "Kriszta@gmail.com", LocalDate.parse("2004-12-10"));
+
+        mockMvc.perform(post("/api/signup")
+                        .content(jacksonObjectMapper.writeValueAsString(newMemberRequest))
+                        .contentType("application/json;charset=UTF-8")
+                )
+                .andDo(print())
+                .andExpect(status().isConflict())
+                .andExpect(result -> assertInstanceOf(UsernameOrEmailAddressAlreadyInUseException.class, result.getResolvedException()))
+                .andExpect(result -> assertEquals("Username or email address is already in use, please choose another one", Objects.requireNonNull(result.getResolvedException()).getMessage()));
+    }
+
+    @Test
+    public void testLogin_FailedWithWrongUsernameOrEmail() throws Exception {
+        MemberLoginRequest memberLoginRequest = new MemberLoginRequest("kriszta", "456456456");
+
+        mockMvc.perform(post("/api/login")
+                        .content(jacksonObjectMapper.writeValueAsString(memberLoginRequest))
+                        .contentType("application/json;charset=UTF-8"))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertInstanceOf(MemberNotFoundWithGivenCredentialsException.class, result.getResolvedException()))
+                .andExpect(result -> assertEquals("User cannot be found with given credentials.", Objects.requireNonNull(result.getResolvedException()).getMessage()));
+    }
+
+    @Test
+    public void testLogin_FailedWithWrongPassword() throws Exception {
+        MemberLoginRequest memberLoginRequest = new MemberLoginRequest("ankriszt", "456");
+
+        mockMvc.perform(post("/api/login")
+                        .content(jacksonObjectMapper.writeValueAsString(memberLoginRequest))
+                        .contentType("application/json;charset=UTF-8"))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertInstanceOf(MemberNotFoundWithGivenCredentialsException.class, result.getResolvedException()))
+                .andExpect(result -> assertEquals("User cannot be found with given credentials.", Objects.requireNonNull(result.getResolvedException()).getMessage()));
+    }
+
+    @Test
+    public void testLogin_Success() throws Exception{
+        MemberLoginRequest memberLoginRequest = new MemberLoginRequest("ankriszt", "456456456");
+
+        mockMvc.perform(post("/api/login")
+                        .content(jacksonObjectMapper.writeValueAsString(memberLoginRequest))
+                        .contentType("application/json;charset=UTF-8"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("ankriszt"));
+
+    }
+
+
 }
--- backend/src/test/resources/data.sql
+++ backend/src/test/resources/data.sql
@@ -1,3 +1,15 @@
 INSERT INTO member_role (role_id, role)
 VALUES (1, 'ROLE_USER'),
        (2, 'ROLE_ADMIN');
+
+INSERT INTO members_roles(member_id, role_id)
+VALUES (1, 1);
+
+INSERT INTO monogram (characters, color_code, monogram_public_id)
+VALUES ('KA', '#000000', 'cbde557a-8129-463d-8e29-81f126b0cebd');
+
+INSERT INTO member (avatar, banner_image, biography, birth_date, date_of_registry, email, first_name,
+                    last_name, member_public_id, password, username, monogram_id)
+VALUES (null, null, null, '2004-12-10', '2025-01-23', 'Kriszta@gmail.com', 'Kriszta', 'Antal',
+        'c5e8f43f-5dd4-4b96-a522-56e757693867', '$2a$10$zCRjgNcjcKkUcrajpTcyw.GnN81Ma/cZDMBB997POK6BnrYmSLqfS',
+        'ankriszt', (SELECT MAX(monogram_id) FROM monogram));
