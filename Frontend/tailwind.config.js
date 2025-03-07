d150ff287760de01cbf96bcff8fb11dae8cc1cdb
Kriszta <144325460+KrisztaAntal@users.noreply.github.com>
2025-03-07 20:13:45 +0100

    install daisyui

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'inner-md': 'inset 0 0.2rem 0.2rem rgba(0, 0, 0, 0.5)',
+      },
+    },
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    base: false,
+    styled: true,
+  },
 }
 
