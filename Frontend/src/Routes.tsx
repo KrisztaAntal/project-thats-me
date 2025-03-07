6332ef23582c9e898b1c058ced0adb82c96689b1
Kriszta <144325460+KrisztaAntal@users.noreply.github.com>
2025-03-07 20:15:50 +0100

    change routes, put Outlet into Layout component

--- Frontend/src/Routes.tsx
+++ Frontend/src/Routes.tsx
@@ -1,26 +1,27 @@
-import { createBrowserRouter, Outlet } from "react-router-dom";
+import {createBrowserRouter} from "react-router-dom";
 import App from "./App";
 import Signup from "./Pages/Signup";
+import Layout from "./Layout.tsx";
 
 export const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: < Outlet />,
-      children: [
+    [
         {
-          path: "/default",
-          element: <App />
+            path: "/",
+            element: < Layout/>,
+            children: [
+                {
+                    path: "default",
+                    element: <App/>
+                }
+            ]
+        },
+        {
+            path: "/signup",
+            element: <Signup/>
+        },
+        {
+            path: "/login",
+            element: <></>
         }
-      ]
-    },
-    {
-      path: "/signup",
-      element: <Signup />
-    },
-    {
-      path: "/signin",
-      element: <></>
-    }
-  ]
+    ]
 )
