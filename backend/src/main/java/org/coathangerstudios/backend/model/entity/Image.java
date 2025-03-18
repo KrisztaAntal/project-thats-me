1312b801a4fc0aa7252074874692a3bb553a4327
Kriszta <144325460+KrisztaAntal@users.noreply.github.com>
2025-03-18 15:37:03 +0100

    create Image entity

---
+++ backend/src/main/java/org/coathangerstudios/backend/model/entity/Image.java
@@ -0,0 +1,28 @@
+package org.coathangerstudios.backend.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Image {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long imageId;
+
+    @UuidGenerator
+    private UUID imagePublicId;
+
+    private String name;
+    private String contentType;
+    @Lob
+    private byte[] imageData;
+
+}
