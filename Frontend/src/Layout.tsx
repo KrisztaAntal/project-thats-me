1b8ba6350c40727b5e64b3d08fe13578ccefef1a
Kriszta <144325460+KrisztaAntal@users.noreply.github.com>
2025-04-01 18:56:55 +0200

    correct navigation in components to use navbar for it

--- Frontend/src/Layout.tsx
+++ Frontend/src/Layout.tsx
@@ -1,8 +1,10 @@
 import {Outlet} from "react-router-dom";
+import NavBar from "./Components/NavBar.tsx";
 
 function Layout(){
     return (
         <div className="min-h-screen">
+            <NavBar/>
             <Outlet/>
         </div>
     )
--- Frontend/src/Pages/MainPage.tsx
+++ Frontend/src/Pages/MainPage.tsx
@@ -1,17 +1,9 @@
-import {useAuth} from "../authProvider/useAuth.tsx";
-import {useNavigate} from "react-router-dom";
-
 function MainPage() {
-    const {logout} = useAuth()
-    const navigate = useNavigate();
 
-    function handleLogout() {
-        logout();
-        navigate("/login");
-    }
+
+
 
     return (<div>
-        <button onClick={handleLogout}>logout</button>
         <h1>Welcome!</h1>
     </div>)
 }
